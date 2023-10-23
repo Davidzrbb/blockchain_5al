274b9cde30754400dd2fb4fc34efce3d253807b3
David <zdavid1@myges.fr>
2023-10-23 16:11:28 +0200

    add idBattle

--- contracts/battle.sol
+++ contracts/battle.sol
@@ -16,6 +16,7 @@ contract Battle is CardHelper {
     BattleData[] public battles;
 
     struct BattleData {
+        uint battleId;
         address player1;
         address player2;
         uint cardIdPlayer1;
@@ -32,7 +33,7 @@ contract Battle is CardHelper {
 
     function _initBattle(uint _cardId) internal returns (uint) {
         addressToCardInBattle[msg.sender][_cardId] = addressToCardInBattle[msg.sender][_cardId].add(1);
-        BattleData memory battle = BattleData(msg.sender, address(0), _cardId, 0, msg.value, 0, new uint[](0), new uint[](0), address(0), address(0), false, "waitP2");
+        BattleData memory battle = BattleData(battles.length + 1, msg.sender, address(0), _cardId, 0, msg.value, 0, new uint[](0), new uint[](0), address(0), address(0), false, "waitP2");
 
         battles.push(battle);
 
@@ -41,31 +42,31 @@ contract Battle is CardHelper {
 
     function _selectBattle(uint _cardId, uint _battleId, uint[] memory _stats) internal returns (bool){
         addressToCardInBattle[msg.sender][_cardId] = addressToCardInBattle[msg.sender][_cardId].add(1);
-        BattleData memory battle = battles[_battleId-1];
+        BattleData memory battle = battles[_battleId - 1];
         battle.player2 = msg.sender;
         battle.cardIdPlayer2 = _cardId;
         battle.amountPlayer2 = msg.value;
         battle.statsPlayer2 = _stats;
         battle.status = "waitP1";
 
-        battles[_battleId-1] = battle;
+        battles[_battleId - 1] = battle;
         return true;
     }
 
     function _confirmBattle(uint _battleId, uint[] memory _stats) internal returns (bool) {
-        BattleData memory battle = battles[_battleId-1];
+        BattleData memory battle = battles[_battleId - 1];
 
         battle.statsPlayer1 = _stats;
 
         battle.status = "confirmed";
 
-        battles[_battleId-1] = battle;
+        battles[_battleId - 1] = battle;
 
         return true;
     }
 
     function _doBattle(uint _battleId) internal returns (bool) {
-        BattleData memory battle = battles[_battleId-1];
+        BattleData memory battle = battles[_battleId - 1];
 
         uint[] memory statsPlayer1 = battle.statsPlayer1;
         uint[] memory statsPlayer2 = battle.statsPlayer2;
@@ -77,19 +78,19 @@ contract Battle is CardHelper {
         uint roundWinPlayer2 = 0;
         uint cardIdWinner = 0;
 
-        for(uint i = 0; i < 3; i++) {
-            if(statsPlayer1[i] > statsPlayer2[i]) {
+        for (uint i = 0; i < 3; i++) {
+            if (statsPlayer1[i] > statsPlayer2[i]) {
                 roundWinPlayer1++;
-            } else if(statsPlayer1[i] < statsPlayer2[i]) {
+            } else if (statsPlayer1[i] < statsPlayer2[i]) {
                 roundWinPlayer2++;
             }
         }
 
-        if(roundWinPlayer1 > roundWinPlayer2){
+        if (roundWinPlayer1 > roundWinPlayer2) {
             battle.winner = battle.player1;
             cardIdWinner = cardIdPlayer1;
-            battle.loser = battle.player2; 
-        } else if(roundWinPlayer1 < roundWinPlayer2) {
+            battle.loser = battle.player2;
+        } else if (roundWinPlayer1 < roundWinPlayer2) {
             battle.winner = battle.player2;
             cardIdWinner = cardIdPlayer2;
             battle.loser = battle.player1;
@@ -100,14 +101,14 @@ contract Battle is CardHelper {
             battle.status = "draw";
             payable(battle.player1).transfer(amountPlayer1);
             payable(battle.player2).transfer(amountPlayer2);
-            battles[_battleId-1] = battle;
+            battles[_battleId - 1] = battle;
             addressToCardInBattle[battle.player1][battle.cardIdPlayer1] = addressToCardInBattle[battle.player1][battle.cardIdPlayer1].sub(1);
             addressToCardInBattle[battle.player2][battle.cardIdPlayer2] = addressToCardInBattle[battle.player2][battle.cardIdPlayer2].sub(1);
             return true;
         }
 
         battle.status = "done";
-        battles[_battleId-1] = battle;
+        battles[_battleId - 1] = battle;
 
         addressToCardInBattle[battle.player1][battle.cardIdPlayer1] = addressToCardInBattle[battle.player1][battle.cardIdPlayer1].sub(1);
         addressToCardInBattle[battle.player2][battle.cardIdPlayer2] = addressToCardInBattle[battle.player2][battle.cardIdPlayer2].sub(1);
@@ -118,42 +119,41 @@ contract Battle is CardHelper {
     }
 
     function _doRoyalty(uint _battleId, uint _totalAmount, uint _cardIdWinner) internal returns (bool){
-       
-        BattleData memory battle = battles[_battleId-1];
+
+        BattleData memory battle = battles[_battleId - 1];
 
         // 10% goes to royalty
         uint256 royalty = _totalAmount.mul(10).div(100);
 
         uint royaltyCardAmount = 0;
-        for(uint i = 0; i < cardIdToOwners[_cardIdWinner].length; i++){
+        for (uint i = 0; i < cardIdToOwners[_cardIdWinner].length; i++) {
             uint amountOfCardId = cardIdToOwnerToAmount[_cardIdWinner][cardIdToOwners[_cardIdWinner][i]];
             royaltyCardAmount = royaltyCardAmount.add(amountOfCardId);
         }
 
-        
-        if(royaltyCardAmount != 0){
+
+        if (royaltyCardAmount != 0) {
             uint royaltyPerCard = royalty.div(royaltyCardAmount);
 
-            for(uint i = 0; i < cardIdToOwners[_cardIdWinner].length; i++){
+            for (uint i = 0; i < cardIdToOwners[_cardIdWinner].length; i++) {
                 uint amountOfCardId = cardIdToOwnerToAmount[_cardIdWinner][cardIdToOwners[_cardIdWinner][i]];
                 uint royaltyAmount = royaltyPerCard.mul(amountOfCardId);
                 payable(cardIdToOwners[_cardIdWinner][i]).transfer(royaltyAmount);
             }
         }
 
-        
         // 90% goes to winner
         uint256 winnerAmount = _totalAmount.sub(royalty);
         payable(battle.winner).transfer(winnerAmount);
 
-        battles[_battleId-1] = battle;
+        battles[_battleId - 1] = battle;
 
         return true;
 
     }
 
     function _cancelBattle(uint _battleId) internal returns (bool) {
-        BattleData memory battle = battles[_battleId-1];
+        BattleData memory battle = battles[_battleId - 1];
 
         payable(battle.player1).transfer(battle.amountPlayer1);
         addressToCardInBattle[battle.player1][battle.cardIdPlayer1] = addressToCardInBattle[battle.player1][battle.cardIdPlayer1].sub(1);
@@ -162,7 +162,7 @@ contract Battle is CardHelper {
 
         battle.status = "canceled";
 
-        battles[_battleId-1] = battle;
+        battles[_battleId - 1] = battle;
 
         return true;
     }
@@ -170,8 +170,8 @@ contract Battle is CardHelper {
     function _getAllBattleUser() internal view returns (BattleData[] memory) {
         BattleData[] memory battlesUser = new BattleData[](battles.length);
         uint counter = 0;
-        for(uint i = 0; i < battles.length; i++) {
-            if(battles[i].player1 == msg.sender || battles[i].player2 == msg.sender) {
+        for (uint i = 0; i < battles.length; i++) {
+            if (battles[i].player1 == msg.sender || battles[i].player2 == msg.sender) {
                 battlesUser[counter] = battles[i];
                 counter++;
             }
